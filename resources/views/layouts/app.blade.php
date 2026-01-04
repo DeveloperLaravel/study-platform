8e3d98d9a17d8c1edad3594c8dc2bb4ebc954e44
Abbes <abbes.dev@gmail.com>
2026-01-04 12:07:43 +0200

    meta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+<meta http-equiv="Content-Security-Policy" content="script-src 'self' 'unsafe-eval' https://jalo.up.railway.app;">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
