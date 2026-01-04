cc6737121e32ea0d2313884b9310c9072b4c085b
Abbes <abbes.dev@gmail.com>
2026-01-04 23:06:22 +0200

    logo

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,4 @@
+<a href="{{ url('/') }}" class="flex items-center space-x-2">
+    <img src="{{ asset('images/bg.png') }}" alt="Logo" class="h-10 w-10">
+    <span class="font-bold text-xl text-gray-800">موقعي</span>
+</a>
